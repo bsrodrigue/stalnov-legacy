2516657533ca7514c706cb4029e9313c56f4cb5e
Badini Rachid Rodrigue <brodrigue@protonmail.com>
2021-09-28 13:00:20 +0000

    progress on signup page

--- src/frontend/static/global/js/main.js
+++ src/frontend/static/global/js/main.js
@@ -1,21 +1,53 @@
 window.addEventListener("DOMContentLoaded", (e) => {
-    console.log("Main Script Loaded");
+    let profileButton = document.querySelector(".profile-button");
+    let sidePanel = document.querySelector(".side-panel");
+    let genderInputs = document.querySelectorAll(".gender-input");
 
-    // DOM Elements
-    let profileButton = document.querySelector('.profile-button');
-    
-
-    let sidePanel = document.querySelector('.side-panel');
-
-
-
-
-    // Event Listeners
-    profileButton.addEventListener('click', e=>{
+    // Toggle Side-Panel
+    profileButton.addEventListener("click", (e) => {
         e.preventDefault();
+        sidePanel.classList.toggle("side-panel-hidden");
+    });
 
-        sidePanel.classList.toggle('side-panel-hidden');
-    })
+    // Signup Form
+    genderInputs.forEach((input) => {
+        function clearGenderRadios() {
+            let genderRadios = document.querySelectorAll(".gender-radio");
+            let genderIcons = document.querySelectorAll(".gender-icon");
 
-    
+            genderRadios.forEach((radio) => {
+                radio.checked = false;
+            });
+
+            genderIcons.forEach((icon) => {
+                icon.classList.remove("gender-is-selected");
+            });
+        }
+        input.addEventListener("click", (e) => {
+            e.preventDefault();
+            let gender = e.target.getAttribute("data-gender");
+            if (gender) {
+                switch (gender) {
+                    case "H":
+                        clearGenderRadios();
+                        document.getElementById("gender-male").checked = true;
+                        document
+                            .querySelector(".fa-male")
+                            .classList.add("gender-is-selected");
+                        break;
+
+                    case "F":
+                        clearGenderRadios();
+                        document.getElementById("gender-female").checked = true;
+                        document
+                            .querySelector(".fa-female")
+                            .classList.add("gender-is-selected");
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        });
+    });
 });
